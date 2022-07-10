3a386c6dce5e09fe996ee776248eacc5ac221daf
Hom <homkafle@gmail.com>
2022-07-10 18:33:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+
 data "aws_ami" "ubuntu_server" {
   most_recent = true
   owners = ["099720109477"]
