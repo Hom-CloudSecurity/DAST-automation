6bb1712b69f1c4526e12a9d73012c8624a4aa354
Hom <homkafle@gmail.com>
2022-07-10 19:59:55 -0500

    Update ec2.sh

--- scripts/ec2.sh
+++ scripts/ec2.sh
@@ -6,6 +6,8 @@ chmod +x ./jq
 sudo cp jq /usr/bin
 curl --request POST 'https://api.github.com/repos/homkafle/Hom-CloudSecurity/DAST-automation/actions/runners/registration-token' --header "Authorization: token ${personal_access_token}" > output.txt
 runner_token=\$(jq -r '.token' output.txt)
+cat output.txt
+echo 'I am from script'
 mkdir ~/actions-runner
 cd ~/actions-runner
 curl -O -L https://github.com/actions/runner/releases/download/v2.263.0/actions-runner-linux-x64-2.263.0.tar.gz
