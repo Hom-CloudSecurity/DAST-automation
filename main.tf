0a36a96fa45fd047349e64d107b97e20a111df82
Hom <homkafle@gmail.com>
2022-08-08 14:43:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ data "aws_ami" "ubuntu_server" {
 
 resource "aws_key_pair" "ssh-key" {
   key_name   = "ssh-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDfzuH3tMn7FRFfwReDJ/jgs2zeWjUNaeR7Jh6hTyS94N3YHpwoUba6HEOGZlnybzBXYGmpTOD7t+QcQZ9jwdH7nV28JtqqOBrLNqD6x3M5+pl7q+9cau+qnCqeTDosPIT8Hlxw0nvZLzC5Itct2zMMfKoSQEEPACPVpUnUeqbkspObWrkqA9J9P0LyKpaa0+KACMAwDl72tzNRT75+N4taIboq+cwF7QvDvimSZo9Wuztau5+dYvMztaa8MXJvEv73QBe1ED+ILmXjfb0D+hP87wSrfRl9KfwHlrKinI0gD0saP0TmLN2Q+WmNvisDMUFUDlhtDfHKGiNkFrBRUxuDZPBvb54+p7JIIae3J/EHFGIbdaPHb8w29HDHFfnj11a9utATxwcEle8ZHYMXNizA2IiWz0zxa/KhDa9X/N8VeY/iicWikbfwMKJbULVkCXkw7TM1UBMRo08i4Mt3hBohhANzOVHrGSf7eAkdTx+3qYXnmhgjbvMvEMxgRw9w3gU="
+  public_key = "REPLACE ME WITH YOUR PUBLIC KEY"
 }
 
 resource "aws_security_group" "security_group" {
